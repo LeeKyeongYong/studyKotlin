7f7293597e95709c957820c7a2805e5187aa7b30
이경용 <sleekydz86@icloud.com>
2025-03-04 16:46:39 +0900

    누락처리

--- src/main/kotlin/com/back/BackApplication.kt
+++ src/main/kotlin/com/back/BackApplication.kt
@@ -2,8 +2,10 @@ package com.back
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class BackApplication
 
 fun main(args: Array<String>) {
