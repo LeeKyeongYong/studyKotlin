ba271fec6a0e62f7f00eed8ce364cb1d8dfbba83
이경용 <sleekydz86@icloud.com>
2025-03-04 15:32:59 +0900

    ref init

---
+++ src/main/kotlin/com/back/BackApplication.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackApplication>(*args)
+}
