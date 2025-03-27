be50200130cbfafc63402c19ec96ce0c7cc34153
이경용 <sleekydz86@naver.com>
2025-03-27 22:53:23 +0900

    feat: sse버전 nofitictaion 기능 학습 Init
    - sse버전 으로 nofitication 기능 구현

---
+++ Nofitication/src/main/kotlin/com/study/nofitication/NofiticationApplication.kt
@@ -0,0 +1,11 @@
+package com.study.nofitication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NofiticationApplication
+
+fun main(args: Array<String>) {
+	runApplication<NofiticationApplication>(*args)
+}
---
+++ Nofitication/src/test/kotlin/com/study/nofitication/NofiticationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.nofitication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NofiticationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
